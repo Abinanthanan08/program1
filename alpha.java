6f57189c9049b54b7930fa13a36a32e3815becd2
Abinanthanan.A <abbianbu@gmail.com>
2023-07-22 17:17:23 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class alpha {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n = s.nextInt();
+        char a = 'A';
+        int b = a;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a);
+            }
+            a++;
+            System.out.println();
+        }
+    }
+}

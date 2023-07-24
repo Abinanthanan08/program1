cbcec18d149ade4cff4f64eb9b6ed13105ac5bde
Abinanthanan.A <139547549+Abinanthanan08@users.noreply.github.com>
2023-07-24 15:02:29 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,17 @@
+package day1_program;
+
+
+public class operations {
+
+	public static void main(String[] args) {
+	int x=5;
+	int z;
+	z=++x + ++x ;
+		
+			System.out.println(z);
+
+		}
+		 
+	}
+
+

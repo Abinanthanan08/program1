2c3de50019ae62f099f0121c5128de9b30ccd75b
Abinanthanan.A <139547549+Abinanthanan08@users.noreply.github.com>
2023-07-24 15:03:12 +0530

    Add files via upload

---
+++ myfirst_pro.java
@@ -0,0 +1,15 @@
+package day1_program;
+
+import java.util.Scanner;
+public class myfirst_pro {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the name:");
+		String name=input.next();
+		System.out.println("you name:"+name);
+		
+		input.close();
+	}
+
+}

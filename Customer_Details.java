84f388447585d9e497e928b0210fb3a990684796
Abinanthanan.A <139547549+Abinanthanan08@users.noreply.github.com>
2023-07-24 14:39:37 +0530

    Update Customer_Details.java

--- Customer_Details.java
+++ Customer_Details.java
@@ -5,9 +5,9 @@ public class Customer_Details {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Customer c1=new Customer();
-		c1.setCid(1008);
-		c1.setC_name("KIG");
-		c1.setAddress("TN_31");
+		c1.setCid(1001);
+		c1.setC_name("ram");
+		c1.setAddress("TN_23");
 		System.out.println(c1);
 		
 	}

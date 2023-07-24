ebe8bf2af987c5cd167727c7d5461453ef8ebb45
Abinanthanan.A <139547549+Abinanthanan08@users.noreply.github.com>
2023-07-24 14:50:33 +0530

    Update Vowles.java

--- Vowles.java
+++ Vowles.java
@@ -4,7 +4,33 @@ public class Vowles {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String Line;
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the String:");
+		Line=s.nextLine();
+		int a=0,e=0,j=0,o=0,u=0;
+		for(int i=0;i<Line.length();i++) {
+			if(Line.charAt(i)=='a'||Line.charAt(i)=='A') {
+				a++;
+			}
+			if(Line.charAt(i)=='e'||Line.charAt(i)=='E') {
+				e++;
+			}
+			if(Line.charAt(i)=='i'||Line.charAt(i)=='I') {
+				j++;
+			}if(Line.charAt(i)=='o'||Line.charAt(i)=='O') {
+				o++;
+			}
+			if(Line.charAt(i)=='u'||Line.charAt(i)=='U') {
+				u++;
+			}
+		
+		}
+		System.out.println("a:"+a+"\n e:"+e+"\n i:"+j+"\n o:"+o+"\n u:"+u);
+		
+
+	}
+		
 
 	}
 

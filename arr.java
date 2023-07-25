c4b09375e8bd055a8faaea5217fce5ebeedcba54
Abinanthanan.A <139547549+Abinanthanan08@users.noreply.github.com>
2023-07-25 09:54:49 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,15 @@
+package day3;
+import java.util.*;
+
+public class arr {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number:");
+		int n=s.nextInt();
+		
+		
+
+	}
+
+}
